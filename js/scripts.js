73a1bc3965c4c56199787c1746736c3bd8911c5d
Keegan Ruebling, Riki Montgomery and javier jimenez <keegan+riki+javier@>
2017-08-24 10:10:43 -0700

    add initial functionality for AI

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@ function Player(player, score, total) {
 
 var player1 = new Player('', 0, 0);
 var player2 = new Player('', 0, 0);
+var computer = new Player('Summer', 0, 0);
 
 function randomInt(min, max) {
   min = Math.ceil(min);
@@ -40,17 +41,46 @@ Player.prototype.hold = function() {
 // front-end logic
 $(document).ready(function() {
 
-// name form
+// human form
 $('form#dice').submit(function(event) {
   event.preventDefault();
   player1.player = $('input#userName1').val();
+  if (player1.player === '') {
+    alert("Did you enter a name for player 1?")
+    return;
+  } else {
+    $('#playButton').show();
+    $('#optionalPlayer').show();
+    $('#name1').append(player1.player);
+    $('#playHuman').hide();
+    $('#playAI').hide();
+  }
+});
+b
+// AI form
+$('#playAI').click(function(event) {
+  event.preventDefault();
+  player1.player = $('input#userName1').val();
+  if (player1.player === '') {
+    alert("Did you enter a name for player 1?")
+    return;
+  } else {
+  $('#name1').append(player1.player);
+  $("#name2").append(computer.player);
+  $('#midRow').show(800);
+  $('#colHide').hide();
+  }
+});
+
+// name form
+$('form#dice3').submit(function(event) {
+  event.preventDefault();
   player2.player = $('input#userName2').val();
   if (player1.player === '' || player2.player === '') {
     alert("Did you enter a name for both players?")
     return;
   } else {
     $('#midRow').show(800);
-    $('#name1').append(player1.player);
     $('#name2').append(player2.player);
     $('#colHide').hide();
   }
@@ -81,9 +111,9 @@ $('#rollButton1').click(function(event) {
     $('#rollScore2').text(player2.rollScore);
     });
 
-    $('#holdButton2').click(function(event) {
-      event.preventDefault();
-      player2.hold();
-      $('#rollTotal2').text(player2.rollTotal);
-    });
+  $('#holdButton2').click(function(event) {
+    event.preventDefault();
+    player2.hold();
+    $('#rollTotal2').text(player2.rollTotal);
+  });
 });
