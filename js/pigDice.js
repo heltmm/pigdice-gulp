df19f27266f44733ed7cb210f48f4898ecfac7e8
Keegan Ruebling and Mark Helt <mark@>
2017-10-10 08:25:10 -0700

    split front end and back end logic

---
+++ js/pigDice.js
@@ -0,0 +1,37 @@
+// back-end logic
+function Player(player, score, total) {
+ this.player = player;
+ this.rollScore = score;
+ this.rollTotal = total;
+}
+
+var player1 = new Player('', 0, 0);
+var player2 = new Player('', 0, 0);
+var computer = new Player('Summer', 0, 0);
+
+function randomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+Player.prototype.diceRoll = function(roll) {
+  if (roll === 1) {
+    this.rollScore = 0;
+    $('#rollResult1, #rollResult2').toggle();
+    $('#rollButton1, #rollButton2').toggle();
+    $('#holdButton1, #holdButton2').toggle();
+  } else {
+    this.rollScore += roll;
+  }
+};
+
+Player.prototype.hold = function() {
+  this.rollTotal += this.rollScore;
+  this.rollScore = 0;
+  if (this.rollTotal >= 100)
+    alert('You win ' + this.player + '!')
+    $('#rollResult1, #rollResult2').toggle();
+    $('#rollButton1, #rollButton2').toggle();
+    $('#holdButton1, #holdButton2').toggle();
+}
